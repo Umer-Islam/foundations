07401f2395736c4f0a4aee4e4f4d05902c2a8336
Umer-Islam <139012668+Umer-Islam@users.noreply.github.com>
2024-01-16 04:28:25 +0500

    Play a round

--- ticTac/main.js
+++ ticTac/main.js
@@ -1,18 +1,20 @@
-console.log("hello World");
+
+// This function makes a random choice.
 let getComputerChoice = () => {
   let choose = ["rock", "paper", "scissors"];
 
   let choice = Math.floor(Math.random() * 3);
   return choose[choice];
 };
+// below variables are declared for use in functions later
 let computerSelection = getComputerChoice();
 let playerSelection = prompt('ROCK,Paper,or Scissors..... DECIDE')
 playerSelection = playerSelection.toLowerCase();
-
+// just for testing 
 console.log({ computerSelection });
 console.log({ playerSelection });
 
-
+// This function takes choice of user and computer and decides who won or is it a draw
 function round (playerSelection, computerSelection) {
    
     if (computerSelection !== playerSelection) {
@@ -21,13 +23,14 @@ function round (playerSelection, computerSelection) {
       (playerSelection === "scissors" && computerSelection === "rock")
     ) {
       return playerSelection === "rock" ? "you won" : "Computer wins";
-    } else if (
-      (playerSelection === "scissors " && computerSelection === "paper") ||
+    }
+     else if (
+      (playerSelection === "scissors" && computerSelection === "paper") ||
       (playerSelection === "paper" && computerSelection === "scissors")
     ) {
       return playerSelection === "scissors" ? "you won" : "Computer wins";
     } else if (
-      (playerSelection === "paper " && computerSelection === "rock") ||
+      (playerSelection === "paper" && computerSelection === "rock") ||
       (playerSelection === "rock" && computerSelection === "paper")
     ) {
       return playerSelection === "paper" ? "you won" : "Computer wins";
@@ -37,5 +40,5 @@ function round (playerSelection, computerSelection) {
     return `Draw`
   }
 };
+//playing a round
 console.log("result of the round: ", round(playerSelection,computerSelection));
-// round
